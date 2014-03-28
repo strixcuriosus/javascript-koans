d92c5c7d94eb192afe08bd802278dd42ebe218fc
Ash Hoover <strixcuriosus@gmail.com>
2014-03-28 14:57:05 -0700

    learned to add multiples of 3 and 5 (imperative and functional) 29/31 koans. 5/6 subjects

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -60,14 +60,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000).reduce(function(a, b){
+      if (b % 3 == 0 || b % 5 == 0) {
+        return a + b;
+      } else {return a};
+    });    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
